da84f520cdc291e703bab8445670e4ee00248c45
tobycaps <tcap745@gmail.com>
2023-08-21 18:11:00 +0800

    Emulator Working

--- app/src/main/java/com/example/budgy_prototype_v01/MainActivity.java
+++ app/src/main/java/com/example/budgy_prototype_v01/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.budgy_prototype_v01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
