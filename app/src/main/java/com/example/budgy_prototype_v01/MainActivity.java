22f3adc7e33126396c9362b17693d716f89ca1b0
tobycaps <tcap745@gmail.com>
2023-09-01 19:56:15 +0800

    design_01

--- app/src/main/java/com/example/budgy_prototype_v01/MainActivity.java
+++ app/src/main/java/com/example/budgy_prototype_v01/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.budgy_prototype_v01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.budgy);
+
+        TextPaint paint = textView.getPaint();
+        float width = paint.measureText("Tianjin, China");
+
+        Shader textShader = new LinearGradient(0, 0, width, textView.getTextSize(),
+                new int[]{
+                        Color.parseColor("#686D86"),
+                        Color.parseColor("#686D86"),
+                        Color.parseColor("#686D86"),
+                        Color.parseColor("#4E5265")
+                }, null, Shader.TileMode.CLAMP);
+        textView.getPaint().setShader(textShader);
     }
 }
