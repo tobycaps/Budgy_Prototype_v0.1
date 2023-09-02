bda3a4c8dd4ea3e863fa990b4937df6888610baf
tobycaps <tcap745@gmail.com>
2023-09-02 13:16:19 +0800

    login crashing

--- app/src/main/java/com/example/budgy_prototype_v01/MainActivity.java
+++ app/src/main/java/com/example/budgy_prototype_v01/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.budgy_prototype_v01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
 import android.os.Bundle;
 import android.text.TextPaint;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.budgy);
+        Button btn = (Button)findViewById(R.id.login);
 
         TextPaint paint = textView.getPaint();
         float width = paint.measureText("Tianjin, China");
@@ -31,5 +35,14 @@ public class MainActivity extends AppCompatActivity {
                         Color.parseColor("#4E5265")
                 }, null, Shader.TileMode.CLAMP);
         textView.getPaint().setShader(textShader);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, loginPage.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/budgy_prototype_v01/loginPage.java
@@ -0,0 +1,14 @@
+package com.example.budgy_prototype_v01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
